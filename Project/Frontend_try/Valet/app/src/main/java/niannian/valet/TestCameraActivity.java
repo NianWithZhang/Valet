45e2defbcf15b8b91076bd5276ee34bfcf7f5e81
酒酿圆子黏黏汤。 <xrui0310@126.com>
2018-12-13 11:40:09 +0800

    camera OK loadCapturedImg OK

--- Project/Frontend_try/Valet/app/src/main/java/niannian/valet/TestCameraActivity.java
+++ Project/Frontend_try/Valet/app/src/main/java/niannian/valet/TestCameraActivity.java
@@ -1,35 +1,77 @@
 package niannian.valet;
 
-import android.app.AlertDialog;
+import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class TestCameraActivity extends AppCompatActivity {
 
+    public static Uri imgUri;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        requestCameraAndStoragePermission();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_camera);
     }
 
+    //申请相机和文件权限
+    public void requestCameraAndStoragePermission(){
+        //申请相机权限
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED&&ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    1);
+        }
+        else if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    1);
+        }
+        else if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    1);
+        }
+    }
 
     public void testCameraButton_Click(View view) throws IOException {
-        AlertDialog alertDialog1 = new AlertDialog.Builder(this)
-                .setTitle("emmmmm")//标题
-                .setMessage(String.valueOf("测试相机"))//内容
-                .setIcon(R.mipmap.ic_launcher)//图标
-                .create();
-        alertDialog1.show();
+//        AlertDialog alertDialog1 = new AlertDialog.Builder(this)
+//                .setTitle("emmmmm")//标题
+//                .setMessage(String.valueOf("测试相机"))//内容
+//                .setIcon(R.mipmap.ic_launcher)//图标
+//                .create();
+//        alertDialog1.show();
 
 //        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK, MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
 //        photoPickerIntent.setType("image/*");
@@ -38,32 +80,44 @@ public class TestCameraActivity extends AppCompatActivity {
         //相机请求码
         final int CAMERA_REQUEST_CODE = 2;
 
-        //用于保存调用相机拍照后所生成的文件
         File tempFile = new File(Environment.getExternalStorageDirectory().getPath(), System.currentTimeMillis() + ".jpg");
         //跳转到调用系统相机
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-        //判断版本
+//        //判断版本
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {   //如果在Android7.0以上,使用FileProvider获取Uri
             intent.setFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-            Uri contentUri = FileProvider.getUriForFile(this, "niannian.valet.fileprovider", tempFile);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, contentUri);
+            imgUri = FileProvider.getUriForFile(this, "niannian.valet.provider", tempFile);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imgUri);
         } else {    //否则使用Uri.fromFile(file)方法获取Uri
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tempFile));
+            imgUri = Uri.fromFile(tempFile);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imgUri);
         }
         startActivityForResult(intent, CAMERA_REQUEST_CODE);
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        ContentResolver cr = this.getContentResolver();
+
+        try {
+            Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(imgUri));
+            ImageView imageView = (ImageView) findViewById(R.id.cameraImgView);
+            /* 将Bitmap设定到ImageView */
+            imageView.setImageBitmap(bitmap);
+        } catch (FileNotFoundException e) {
+            Log.e("Exception", e.getMessage(),e);
+        }
 //        switch (requestCode) {
 //            // 调用相机后返回
 //            case CAMERA_REQUEST_CODE:
 //                if (resultCode == RESULT_OK) {
 //                    //用相机返回的照片去调用剪裁也需要对Uri进行处理
 //                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-//                        Uri contentUri = FileProvider.getUriForFile(Main2Activity.this, "com.hansion.chosehead", tempFile);
-//                        cropPhoto(contentUri);//裁剪图片
+//                        Uri imgUri = FileProvider.getUriForFile(Main2Activity.this, "com.hansion.chosehead", tempFile);
+//                        cropPhoto(imgUri);//裁剪图片
 //                    } else {
 //                        cropPhoto(Uri.fromFile(tempFile));//裁剪图片
 //                    }
