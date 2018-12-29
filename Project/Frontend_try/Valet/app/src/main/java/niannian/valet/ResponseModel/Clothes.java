2f06e615d07e06e2abf43e98e53d235ed38d820f
酒酿圆子黏黏汤。 <xrui0310@126.com>
2018-12-29 23:05:26 +0800

    Update Clothes.java

--- Project/Frontend_try/Valet/app/src/main/java/niannian/valet/ResponseModel/Clothes.java
+++ Project/Frontend_try/Valet/app/src/main/java/niannian/valet/ResponseModel/Clothes.java
@@ -1,5 +1,7 @@
 package niannian.valet.ResponseModel;
 
+import android.widget.Switch;
+
 import niannian.valet.R;
 
 
@@ -14,5 +16,18 @@ public class Clothes implements UrlPic {
     public int type;
 
     //获取衣物的图片路径
-    public String url(){return R.string.server_url+"\\clothespic\\"+String.valueOf(id)+".jpg";}
+    public String url(){return R.string.server_url+"\\clothespics\\"+String.valueOf(id)+".jpg";}
+
+    //获取衣物类型名称
+    public String getTypeName(){
+        switch(type){
+            case 0:return "帽子";
+            case 1:return "外套";
+            case 2:return "内衫";
+            case 3:return "裤裙";
+            case 4:return "袜子";
+            case 5:return "鞋子";
+        }
+        return null;
+    }
 }
