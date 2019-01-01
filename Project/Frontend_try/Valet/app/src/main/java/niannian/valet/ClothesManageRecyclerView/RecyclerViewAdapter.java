836325e34d864e5b33aaf8fc7d9b060bc79112c8
酒酿圆子黏黏汤。 <xrui0310@126.com>
2019-01-01 15:51:59 +0800

    checkbox id ok

--- Project/Frontend_try/Valet/app/src/main/java/niannian/valet/ClothesManageRecyclerView/RecyclerViewAdapter.java
+++ Project/Frontend_try/Valet/app/src/main/java/niannian/valet/ClothesManageRecyclerView/RecyclerViewAdapter.java
@@ -1,13 +1,18 @@
 package niannian.valet.ClothesManageRecyclerView;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import niannian.valet.R;
 import niannian.valet.ResponseModel.ClothesResponse;
@@ -15,10 +20,42 @@ import niannian.valet.ResponseModel.ClothesResponseList;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyHolder> {
     ClothesResponseList Clothes;
+    private Context mContext;
+    //private List<String> lists;
+    private HashMap<Integer,Boolean> Maps=new HashMap<Integer,Boolean>();
+    private HashMap<Integer,Boolean>AllMaps=new HashMap<Integer,Boolean>();
+    public RecyclerViewOnItemClickListener onItemClickListener;
 
     public RecyclerViewAdapter(ClothesResponseList ClothesList){
+
         this.Clothes=ClothesList;
+        for (int i = 0; i < Clothes.clothes.length; i++) {
+            Maps.put(i, false);
+        }
     }
+
+    //获取最终的map存储数据
+    public Map<Integer,Boolean> getMap(){
+        return Maps;
+    }
+
+    //后续扩展 - 获取最终的map存储数据
+//   public Map<Integer,Boolean>getAllMap(){
+//        return AllMaps;
+//    }
+
+    //点击item选中CheckBox
+    public void setSelectItem(int position) {
+        //对当前状态取反
+        if (Maps.get(position)) {
+            Maps.put(position, false);
+        } else {
+            Maps.put(position, true);
+        }
+        notifyItemChanged(position);
+    }
+
+
 
     @Override
     public MyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -29,25 +66,66 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     @Override
-    public void onBindViewHolder(MyHolder holder, int position) {
+    public void onBindViewHolder(MyHolder holder, final int position) {
         ClothesResponse currentClothes=Clothes.clothes[position];
         //holder.imageView.setImageResource(currentClothes.image);
         holder.textView.setText(currentClothes.name);
+        holder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Maps.put(position,isChecked);
+            }
+        });
+        if(Maps.get(position)==null){
+            Maps.put(position,false);
+        }
+
+        holder.checkBox.setChecked(Maps.get(position));
+
+
+        //之后扩展使用
+        AllMaps.put(position,true);
+
+
     }
 
 
 
     @Override
     public int getItemCount() {
+
         return Clothes.clothes.length;
     }
+
+
+
     class MyHolder extends RecyclerView.ViewHolder{
         //ImageView imageView;
         TextView textView;
+        CheckBox checkBox;
+        private RecyclerViewOnItemClickListener listener;
         public MyHolder(View itemView) {
             super(itemView);
+            this.listener = onItemClickListener;
             textView=(TextView)itemView.findViewById(R.id.clothesNameText);
             //imageView=(ImageView) itemView.findViewById(R.id.clothesImage);
+            checkBox = (CheckBox) itemView.findViewById(R.id.checkboxChooseClothes);
         }
+
+        public void onClick(View v) {
+            if (listener != null) {
+                listener.onItemClickListener(v, getPosition());
+            }
+        }
+
+    }
+    public void setItemClickListener(RecyclerViewOnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+
+    public interface RecyclerViewOnItemClickListener {
+        //点击事件
+        void onItemClickListener(View view, int position);
     }
 }
--- Project/Frontend_try/Valet/app/src/main/java/niannian/valet/ResponseModel/ClothesResponseList.java
+++ Project/Frontend_try/Valet/app/src/main/java/niannian/valet/ResponseModel/ClothesResponseList.java
@@ -5,13 +5,16 @@ import java.util.ArrayList;
 
 public class ClothesResponseList {
     public ClothesResponse[] clothes;
+
     public ClothesResponseList(){
         int id=1;
         String name="abc";
-        int type=1;
 
         clothes = new ClothesResponse[10];
-        ClothesResponse first=new ClothesResponse(id,name,type);
-        clothes[0]=first;
+//        ClothesResponse first=new ClothesResponse(id,name,type);
+
+        for(int i=0;i<10;i++)
+            clothes[i]=new ClothesResponse(i,name+String.valueOf(i),1);
+
     }
 }
